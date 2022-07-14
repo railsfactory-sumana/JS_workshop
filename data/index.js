a6d942fa6f453bb061b88ea5c340963ab504fac4
railsfactory-sumana <sumana@railsfactory.org>
2022-07-14 14:18:06 +0530

    n

---
+++ data/index.js
@@ -0,0 +1,30 @@
+const students = [
+    {
+      id: 1,
+      name: 'parasmita',
+      marks: {
+        ennglish: 80,
+        science: 70,
+        social: 60
+      }
+    },
+    {
+      id: 2,
+      name: 'piya',
+      marks: {
+        ennglish: 85,
+        science: 75,
+        social: 62
+      }
+    },
+    {
+      id: 3,
+      name: 'rahul',
+      marks: {
+        ennglish: 83,
+        science: 72,
+        social: 63
+      }
+    }
+  ]
+module.exports = students
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const data = require("./data");
+const transformStudent =(students) => {
+    const {english, social, science} = students.marks;
+   return `${students.id}, ${students.name}, ${english}`
 
-const main=() => {
-    data.map((students) => {
-        const marks = students.marks
-        console.log(students.id, students.name, students.marks)
-    })
 }
+
+const logResult = (result) => {console.log(result)}
+const main=() => {
+    data.map(transformStudent).map(logResult);
+    
+};
 
 main()

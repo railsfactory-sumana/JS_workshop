f5f21191ce0935af0917a355416bf5222f5c5b2b
railsfactory-sumana <sumana@railsfactory.org>
2022-07-14 15:00:44 +0530

    refactor calculate total mark

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const students = require("./data");
 const data = require("./data");
 
 const calculateTotal = (student) => {
     const marks = Object.values(student.marks)
-    student.total = marks.reduce((mark, value) => value + mark , 0)
-    return student
-
-
+    const total = marks.reduce((mark, value) => value + mark , 0)
+    return {...student, total }
 }
+
+const refactorCalculateTotal = (student) => 
+({
+    ...student,
+     total:  Object.values(student.marks).reduce((mark, value) => value + mark , 0)
+})
+
+
 const transformStudent =(student) => {
     const {id, name, total, marks: { english, social, science}} = student;
    return ` ${id}, ${name}, ${english} ${social} ${science}, ${total}`
@@ -17,6 +24,7 @@ const logResult = (result) => console.log(result)
 const main=() => {
     data
     .map(calculateTotal)
+    .map(refactorCalculateTotal)
     .map(transformStudent)
     .map(logResult)
     
